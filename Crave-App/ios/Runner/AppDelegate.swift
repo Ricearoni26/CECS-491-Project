9f5fcc65a9c66dd97bab632a751f133dbb1fb6fb
Ricearoni26 <joey.rice01@student.csulb.edu>
2022-11-06 22:25:13 -0800

    adding map configuration

--- Crave-App/ios/Runner/AppDelegate.swift
+++ Crave-App/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDPYy605byX1BV3GUuDKo4064qRSGiOsfs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
