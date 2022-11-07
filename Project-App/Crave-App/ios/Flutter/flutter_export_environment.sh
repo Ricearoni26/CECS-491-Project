6ae8db68887d1030752ae07f0d266842a7b2cdf2
Ricearoni26 <joey.rice01@student.csulb.edu>
2022-11-06 16:08:37 -0800

    changes

---
+++ Project-App/Crave-App/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/josephrice/FlutterDev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/josephrice/Desktop/Github-Files/CECS-491-Project/Project_Folder/Project-App/Crave-App"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/josephrice/Desktop/Github-Files/CECS-491-Project/Project_Folder/Project-App/Crave-App/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/josephrice/Desktop/Github-Files/CECS-491-Project/Project_Folder/Project-App/Crave-App/.dart_tool/package_config.json"
